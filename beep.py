bdaff658c88a3efe7768326679de51cb0455caf7
Adrian <adrianzjones@gmail.com>
2023-06-27 20:15:02 -0700

    get version one of the consciousness meter up and running, complete with an input script

--- beep.py
+++ beep.py
@@ -4,7 +4,7 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 
 pygame.mixer.init()
-sound = pygame.mixer.Sound('beep.mp3')
+sound = pygame.mixer.Sound('button.mp3')
 sound.play()
 pygame.time.wait(int(sound.get_length() * 1000))
 pygame.mixer.quit()
--- install.sh
+++ install.sh
@@ -1,28 +1,25 @@
-!#/bin/bash
+#!/bin/bash
 
-SCRIPT="cons.py"
+SCRIPT="consc.py"
 COMMAND="consornot"
-INSTALL_LOC="/user/local/bin"
+INSTALL_LOC="/usr/local/bin"
 
-# making sure all the Bash scripts have the right permissions
-chmod +x 2end_credits_head.sh
-chmod +x 2illusionist.sh
-chmod +x 2panpsychist.sh
-chmod +x animation.sh
-chmod +x animation2.sh
-chmod +x bar.sh
-chmod +x booting.sh
-chmod +x closing_ani.sh
-chmod +x end_credits_head.sh
-chmod +x illusionist.sh
-chmod +x menu.sh
-chmod +x panpsychist.sh
-chmod +x second_menu.sh
-chmod +x shut_down.py
-chmod +x shut_down.sh
-chmod +x shutdown.mp3
-chmod +x third_menu.sh
+# checking for sudo permissions
+# if [ "$EUID" -ne 0 ]
+# then echo "Please run this script with sudo."
+#    exit
+# fi
 
+# check if files are there
+# for file in cons.py "${OTHER_PYTHON_SCRIPTS[@]}" "{$BASH_SCRIPTS[@]}" "${REM_FILES[@]}"; do
+#   if [ ! -f "$file" ]; then
+#       echo "File $file not found! Please make sure you are in the right directory."
+#       exit 1
+### fi
+#done
+
+# making sure its in the right location; might be redundant
+# mv consc.py $INSTALL_LOC
 
 # check for python3
 if ! command -v python3 &> /dev/null
@@ -44,6 +41,44 @@ pip3 freeze | grep -q '^inquirer=' || pip3 install inquirer
 pip3 freeze | grep -q '^pygame=' || pip3 install pygame
 
 # Make the Python script executable
-chmod +x $SCRIPT
+chmod +x $INSTALL_LOC/$SCRIPT
 
-# Remove the .py extension and move the script to the 
+# making sure all the Bash scripts have the right permissions
+#############################################################
+
+
+# list of all the remaining python scripts
+OTHER_PYTHON_SCRIPTS=("2end_credits_head.py" "ani.py" "animation2.py" "beep.py" "credits.py" "digital_rain.py" "end_credits_head.py" "heading_end.py" "heading_ill.py" "heading_pan.py" "loading_bar.py" "matrix_rain.py" "menu.py" "prompt.py" "rain.py" "second_menu.py" "second_menu_pan.py" "shut_down.py" "sound.py" "status_ill.py" "status_pan.py" "status_print.py" "third_menu.py")
+
+# list of python scripts
+for SCRIPT_PY in "${OTHER_PYTHON_SCRIPTS[@]}"; do
+    chmod +x $SCRIPT_PY
+    cp $SCRIPT_PY $INSTALL_LOC/$SCRIPT_PY
+done
+
+
+# list of bash scripts
+BASH_SCRIPTS=("2end_credits_head.sh" "2illusionist.sh" "2panpsychist.sh" "animation.sh" "animation2.sh" "bar.sh" "booting.sh" "closing_ani.sh" "end_credits_head.sh" "illusionist.sh" "menu.sh" "panpsychist.sh" "second_menu.sh" "shut_down.sh" "third_menu.sh")
+
+# make bash scripts executable, then move them to install location
+for SCRIPT_SH in "${BASH_SCRIPTS[@]}"; do
+    chmod +x $SCRIPT_PY
+    cp $SCRIPT_SH $INSTALL_LOC/$SCRIPT_SH
+done
+
+
+# list of sound effects and other remaining files
+REM_FILES=("antipat.mp3" "bach.mp3" "beep.mp3" "beep.ogg" "button.mp3" "conscious.mp3" "credits.txt" "info.txt" "not_conscious.mp3" "shutdown.mp3" "ufo.wav" "user_object.txt")
+
+# put them in the install location
+for REM_FILE in "${REM_FILES[@]}"; do
+    cp $REM_FILE $INSTALL_LOC/$REM_FILE
+done
+
+
+# creat a symbolic link for the command
+ln -s $INSTALL_LOC/$SCRIPT $INSTALL_LOC/$COMMAND
+
+# last message
+echo "Install complete. You can now run the consciousness meter by typing the command 'consornot' into your terminal."
+
