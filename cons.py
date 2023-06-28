7f7d60f92aad464e468e8b8ccd7bcbb9a6ab5943
Adrian <adrianzjones@gmail.com>
2023-06-27 20:56:55 -0700

    everything in order to push this to github

---
+++ cons.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# author: adrian jones
+
+import inquirer
+import subprocess
+import os
+import time
+
+# making sure that the relevant BASH scripts are executable
+os.chmod('./illusionist.sh', 0o755)
+os.chmod('./panpsychist.sh', 0o755)
+
+print("Navigate with your arrow keys.")
+questions = [
+    inquirer.List('options',
+                  message="\033[1m" +  "RUN CONSCIOUSNESS METER IN: " + "\033[0m",
+                  choices=["illusionist mode", "panpsychist mode"],
+            ),
+]
+
+answers = inquirer.prompt(questions)
+print('Booting consciousness meter up in', answers['options'])
+
+if answers['options'] == 'illusionist mode':
+    subprocess.call(['./illusionist.sh'])
+elif answers['options'] == 'panpsychist mode':
+    subprocess.call(['./panpsychist.sh'])
