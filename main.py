6d48ae37b4584952f6aa9e738e98f140b40ba61c
Adrian Jones <adrianzjones@gmail.com>
2023-07-05 11:26:52 -0700

    updated the main.py file to import all the other files

--- main.py
+++ main.py
@@ -15,7 +15,31 @@ import pygame
 
 # other python files to import
 import opening_script
-
+import ani
+import animation2
+import beep
+import cons
+import credits
+import digital_rain
+import end_credits_head
+import heading_end
+import heading_ill
+import heading_pain
+import loading_bar
+import matrix_rain
+import menu
+import opening_script
+import prompt
+import rain
+import second_menu
+import second_menu_pan
+import shut_down
+import sound
+import status_ill
+import status_pan
+import status_print
+import third_menu
+import two_end_credits_head
 
 def main():
     opening_script.opening()
